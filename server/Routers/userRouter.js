3a60e2f81798efd6a9a7383fa83cf798f17005a2
Snehal-Av <113681654+Snehal-Av@users.noreply.github.com>
2024-10-17 14:20:15 +0530

    Update userRouter.js

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const { register, logIn, user, dashUser } = require('../controllers/userAuthController')
-const authMiddleware = require('../middleware/authMiddleware')
+// const authMiddleware = require('../middleware/authMiddleware')
 const router=express.Router()
 
 router.post("/register",register)
 
 router.post("/login",logIn)
-router.get("/user",authMiddleware,dashUser)
+// router.get("/user",authMiddleware,dashUser)
 
 module.exports=router
